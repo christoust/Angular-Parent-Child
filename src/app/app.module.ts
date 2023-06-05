bc251e0853f15dedf31323766f99d1b112151d50
Christo <245052@ust.com>
2023-06-05 14:20:54 +0530

    first-commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MovieDashboardComponent } from './movie-dashboard/movie-dashboard.component';
+import { MovieTableComponent } from './movie-table/movie-table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MovieDashboardComponent,
+    MovieTableComponent
+
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
